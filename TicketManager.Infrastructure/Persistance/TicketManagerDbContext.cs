55e880bdad526c17b3b31b2277042aaf21bc1a52
lzlocki <lzlocki@poczta.fm>
2024-07-06 08:39:32 +0200

    add dbcontext file

---
+++ TicketManager.Infrastructure/Persistance/TicketManagerDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TicketManager.Infrastructure.Persistance
+{
+    class TicketManagerDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost,1433;Initial Catalog=TicketManager;User ID=SA;Password=TicketManager!1;TrustServerCertificate=true");
+        }
+    }
+}
