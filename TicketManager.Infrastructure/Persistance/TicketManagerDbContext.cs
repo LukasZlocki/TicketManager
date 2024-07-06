636712cdf01fdb8dc56e923695527ad6bd84ecb3
lzlocki <lzlocki@poczta.fm>
2024-07-06 09:33:14 +0200

    add models as database tables

--- TicketManager.Infrastructure/Persistance/TicketManagerDbContext.cs
+++ TicketManager.Infrastructure/Persistance/TicketManagerDbContext.cs
@@ -1,11 +1,22 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using TicketManager.Models;
+using TicketManager.Models.Models;
 
 namespace TicketManager.Infrastructure.Persistance
 {
     class TicketManagerDbContext : DbContext
     {
         public DbSet<Department> Departments { get; set; }
+        public DbSet<FactoryLocation> FactoryLocations { get; set; }
+        public DbSet<LabLocation> LabLocations { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductDisplacement> ProductDisplacements { get; set; }
+        public DbSet<ProductType> ProductTypes { get; set; }
+        public DbSet<ReportStructure> ReportStructures { get; set; }
+        public DbSet<ReportType> ReportTypes { get; set; }
+        public DbSet<Test> Tests { get; set; }
+        public DbSet<Ticket> Tickets { get; set; }
+        public DbSet<TicketStatus> TicketStatuses { get; set; }
+        public DbSet<TicketTest> TicketTests { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
