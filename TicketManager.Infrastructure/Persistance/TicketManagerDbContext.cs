237bcc9cec5e44b706151742972f089f67dafd36
lzlocki <lzlocki@poczta.fm>
2024-07-06 09:23:36 +0200

    add dbcontext changes before pulling from main repo

--- TicketManager.Infrastructure/Persistance/TicketManagerDbContext.cs
+++ TicketManager.Infrastructure/Persistance/TicketManagerDbContext.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using TicketManager.Models;
 
 namespace TicketManager.Infrastructure.Persistance
 {
     class TicketManagerDbContext : DbContext
     {
+        public DbSet<Department> Departments { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Server=localhost,1433;Initial Catalog=TicketManager;User ID=SA;Password=TicketManager!1;TrustServerCertificate=true");
