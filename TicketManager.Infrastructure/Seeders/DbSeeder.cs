e5a4e10367e32b527244ca0b009be5d3c06fc754
lzlocki <lzlocki@poczta.fm>
2024-07-07 12:43:34 +0200

    add seed Departments db

---
+++ TicketManager.Infrastructure/Seeders/DbSeeder.cs
@@ -0,0 +1,49 @@
+﻿using TicketManager.Infrastructure.Persistance;
+using TicketManager.Models.Models;
+
+namespace TicketManager.Infrastructure.Seeders
+{
+    public class DbSeeder
+    {
+        private readonly TicketManagerDbContext _dbContext;
+
+        public DbSeeder (TicketManagerDbContext dbContext) 
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task Seed()
+        {
+            // Seed Departments db
+            if(await _dbContext.Database.CanConnectAsync())
+            {
+                if (!_dbContext.Departments.Any())
+                {
+                    var department1 = new Department()
+                    {
+                        DepartmentDescription = "Test Department 1",
+                        FactoryLocationId = 1
+                    };
+                    _dbContext.Departments.Add(department1);
+
+                    var department2 = new Department()
+                    {
+                        DepartmentDescription = "Test Department 2",
+                        FactoryLocationId = 2
+                    };
+                    _dbContext.Departments.Add(department2);
+
+                    var department3 = new Department()
+                    {
+                        DepartmentDescription = "Test Department 3",
+                        FactoryLocationId = 3
+                    };
+                    _dbContext.Departments.Add(department3);
+
+                    await _dbContext.SaveChangesAsync();
+                }
+            }
+        }
+
+    }
+}
