1de2b962cb245862e91d9e01a7b0ef917f0a4efc
lzlocki <lzlocki@poczta.fm>
2024-07-09 18:58:50 +0200

    add interface for department services

---
+++ TicketManager.Services/Department_service/IDepartmentService.cs
@@ -0,0 +1,10 @@
+﻿using TicketManager.Models.Models;
+
+namespace TicketManager.Services.Department_service
+{
+    public interface IDepartmentService
+    {
+        public List<Department> GetAllDepartmentsByFactoryLocationId(int factoryLocationId);
+        public Department GetDepartmentByDepartmentId(int departmentId);
+    }
+}
