644fbdd2a0697e31ae1ca22412be9caea1f4c677
lzlocki <lzlocki@poczta.fm>
2024-07-09 18:53:32 +0200

    add interface for factory location service

---
+++ TicketManager.Services/FactoryLocation_service/IFactoryLocationService.cs
@@ -0,0 +1,10 @@
+﻿using TicketManager.Models.Models;
+
+namespace TicketManager.Services.FactoryLocation_service
+{
+    public interface IFactoryLocationService
+    {
+        public List<FactoryLocation> GetAllFactoryLocations();
+        public FactoryLocation GetFactoryLocationByFactoryLocationId(int factoryLocationId);
+    }
+}
