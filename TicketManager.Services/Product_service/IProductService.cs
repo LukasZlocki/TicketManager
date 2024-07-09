e993a4d7cffc4f798847379bf51507b2f8538cc8
lzlocki <lzlocki@poczta.fm>
2024-07-09 19:04:18 +0200

    add product service interface

---
+++ TicketManager.Services/Product_service/IProductService.cs
@@ -0,0 +1,10 @@
+﻿using TicketManager.Models.Models;
+
+namespace TicketManager.Services.Product_service
+{
+    public interface IProductService
+    {
+        public List<Product> GetAllProductFamilies();
+        public Product GetProductById(int priductId);
+    }
+}
