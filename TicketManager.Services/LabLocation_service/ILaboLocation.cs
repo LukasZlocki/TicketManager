b4296e3d43ad6805cac35f29ff6e5ae78bd2365a
lzlocki <lzlocki@poczta.fm>
2024-07-09 18:50:28 +0200

    add interface for lab location service

---
+++ TicketManager.Services/LabLocation_service/ILaboLocation.cs
@@ -0,0 +1,10 @@
+﻿using TicketManager.Models.Models;
+
+namespace TicketManager.Services.LabLocation_service
+{
+    public interface ILaboLocation
+    {
+        public List<LabLocation> GetAllLabLocations();
+        public LabLocation GetLablocationByLabLocationId(int labLocationId);
+    }
+}
