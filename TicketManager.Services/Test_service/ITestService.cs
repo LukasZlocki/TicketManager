9b79f93223652e1cafc1077f95e3800424702617
lzlocki <lzlocki@poczta.fm>
2024-07-09 19:27:48 +0200

    add interface for test services

---
+++ TicketManager.Services/Test_service/ITestService.cs
@@ -0,0 +1,10 @@
+﻿using TicketManager.Models.Models;
+
+namespace TicketManager.Services.Test_service
+{
+    public interface ITestService
+    {
+        public List<Test> GetTestsByLabLocation(int labLocationId);
+        public Test GetTestById(int testId); 
+    }
+}
