e3a139e964ad4fa7d042f25e4f7d3a8cb71e4770
lzlocki <lzlocki@poczta.fm>
2024-07-09 19:19:13 +0200

    add product displacement interface for service

---
+++ TicketManager.Services/ProductDisplacement_service/IProductDisplacementService.cs
@@ -0,0 +1,10 @@
+﻿using TicketManager.Models.Models;
+
+namespace TicketManager.Services.ProductDisplacement_service
+{
+    public interface IProductDisplacementService
+    {
+        public List<ProductDisplacement> GetProductDisplacementsByProductFamilyId(int productFamilyId);
+        public ProductDisplacement GetProductDisplacementById(int productDisplacementId);
+    }
+}
