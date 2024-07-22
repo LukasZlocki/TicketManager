77eba50de5bfa0620e07ad0b37a2d8d0eee591fb
lzlocki <lzlocki@poczta.fm>
2024-07-22 22:16:14 +0200

    add lab location controller

---
+++ TicketManager.Api/Controllers/LabLocationController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TicketManager.Services.LabLocation_Services;
+
+namespace TicketManager.Api.Controllers
+{
+    public class LabLocationController : Controller
+    {
+        private readonly ILabLocationService _labLocationService;
+
+        public LabLocationController(ILabLocationService labLocationService)
+        {
+            _labLocationService = labLocationService;
+        }
+
+        // GET
+        [HttpGet("api/lablocation")]
+        public ActionResult GetAllLabLocations()
+        {
+            var locations = _labLocationService.GetAllLabLocations();
+            return Ok(locations);
+        }
+
+        // GET
+        [HttpGet("api/lablocation/{id}")]
+        public ActionResult GetLabLocationById(int id)
+        {
+            var locations = _labLocationService.GetLablocationByLabLocationId(id);
+            return Ok(locations);
+        }
+
+    }
+}
