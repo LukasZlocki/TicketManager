49a5318f92c82a8b5909f508b8b0e61741b3e402
lzlocki <lzlocki@poczta.fm>
2024-07-22 20:07:40 +0200

    implement interface for department services

---
+++ TicketManager.Services/Department_Services/DepartmentService.cs
@@ -0,0 +1,39 @@
+﻿using TicketManager.Infrastructure.Persistance;
+using TicketManager.Models.Models;
+
+namespace TicketManager.Services.Department_Services
+{
+    public class DepartmentService : IDepartmentService
+    {
+        private readonly TicketManagerDbContext _db;
+
+        public DepartmentService(TicketManagerDbContext db)
+        {
+            _db = db;
+        }
+
+        /// <summary>
+        /// Returns list of departments object which are located in factory by factory id. 
+        /// </summary>
+        /// <param name="factoryLocationId"></param>
+        /// <returns>List of Department</returns>
+        public List<Department> GetAllDepartmentsByFactoryLocationId(int factoryLocationId)
+        {
+            var service = _db.Departments
+                .Where(id => id.FactoryLocationId == factoryLocationId)
+                    .ToList();
+            return service;
+        }
+
+        /// <summary>
+        /// Returns department object by department id.
+        /// </summary>
+        /// <param name="departmentId"></param>
+        /// <returns>Department object</returns>
+        public Department GetDepartmentByDepartmentId(int departmentId)
+        {
+            var service = _db.Departments.FirstOrDefault(id => id.DepartmentId == departmentId);
+            return service ?? new Department();
+        }
+    }
+}
