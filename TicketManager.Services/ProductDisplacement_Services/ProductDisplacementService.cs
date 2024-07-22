cae3618c3f685554f5d141099d339792f36c7fbb
lzlocki <lzlocki@poczta.fm>
2024-07-22 21:06:20 +0200

    implement interface for product displacement services

---
+++ TicketManager.Services/ProductDisplacement_Services/ProductDisplacementService.cs
@@ -0,0 +1,27 @@
+﻿using TicketManager.Infrastructure.Persistance;
+using TicketManager.Models.Models;
+
+namespace TicketManager.Services.ProductDisplacement_Services
+{
+    public class ProductDisplacementService : IProductDisplacementService
+    {
+        private readonly TicketManagerDbContext _db;
+
+        public ProductDisplacementService(TicketManagerDbContext db)
+        {
+            _db = db;
+        }
+
+        public ProductDisplacement GetProductDisplacementById(int productDisplacementId)
+        {
+            var service = _db.ProductDisplacements.FirstOrDefault(id => id.ProductDisplacementId == productDisplacementId);
+            return service ?? new ProductDisplacement();
+        }
+
+        public List<ProductDisplacement> GetProductDisplacementsByProductFamilyId(int productFamilyId)
+        {
+            var service = _db.ProductDisplacements.Where(pr => pr.ProductId == productFamilyId).ToList();
+            return service;
+        }
+    }
+}
