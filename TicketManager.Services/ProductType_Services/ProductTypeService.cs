14540f69e68fc667c0887d9cd3bf4995f13ea152
lzlocki <lzlocki@poczta.fm>
2024-07-22 21:11:13 +0200

    implement interface for product type services

---
+++ TicketManager.Services/ProductType_Services/ProductTypeService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicketManager.Infrastructure.Persistance;
+using TicketManager.Models.Models;
+
+namespace TicketManager.Services.ProductType_Services
+{
+    public class ProductTypeService : IProductTypeService
+    {
+        private readonly TicketManagerDbContext _db;
+
+        public ProductTypeService(TicketManagerDbContext db)
+        {
+            _db = db;
+        }
+
+        public List<ProductType> GetAllProductTypesByFamilyId(int familyId)
+        {
+            var service = _db.ProductTypes
+                .Where(id => id.ProductId == familyId)
+                    .ToList();
+            return service;
+        }
+
+        public ProductType GetProductTypeById(int productTypeId)
+        {
+            var service = _db.ProductTypes.FirstOrDefault(id => id.ProductTypeId == productTypeId);
+            return service ?? new ProductType();
+        }
+    }
+}
