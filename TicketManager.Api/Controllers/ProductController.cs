329d0914dafefb0febd9e3aa5bd3cb33347638d5
lzlocki <lzlocki@poczta.fm>
2024-07-23 21:39:48 +0200

    add product familly controller

---
+++ TicketManager.Api/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TicketManager.Services.Product_Services;
+
+namespace TicketManager.Api.Controllers
+{
+    [ApiController]
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet("api/productfamily")]
+        public ActionResult GetAllProductFamilies()
+        {
+            var productFamilies = _productService.GetAllProductFamilies();
+            return Ok(productFamilies);
+        }
+    }
+}
