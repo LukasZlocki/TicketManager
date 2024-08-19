ff59f5c1064d94c5554f2bd47297d8b3307b024b
lzlocki <lzlocki@poczta.fm>
2024-08-19 17:05:55 +0200

    update app db context

--- TicketManager.Infrastructure/Persistance/TicketManagerDbContext.cs
+++ TicketManager.Infrastructure/Persistance/TicketManagerDbContext.cs
@@ -11,26 +11,29 @@ namespace TicketManager.Infrastructure.Persistance
 
         }
         //*/
-            
+
         public DbSet<Department> Departments { get; set; }
         public DbSet<FactoryLocation> FactoryLocations { get; set; }
         public DbSet<LabLocation> LabLocations { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductDisplacement> ProductDisplacements { get; set; }
         public DbSet<ProductType> ProductTypes { get; set; }
+        public DbSet<ProductFamily> ProductFamilies { get; set; }
         public DbSet<ReportStructure> ReportStructures { get; set; }
         public DbSet<ReportType> ReportTypes { get; set; }
         public DbSet<Test> Tests { get; set; }
         public DbSet<Ticket> Tickets { get; set; }
         public DbSet<TicketStatus> TicketStatuses { get; set; }
         public DbSet<TicketTest> TicketTests { get; set; }
+        public DbSet<TestParameter> TestParameters { get; set; }
+        public DbSet<TicketTestParameter> TicketTestParameters { get; set; }
 
         /* //in case of migration
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server=localhost,1433;Initial Catalog=TicketManager;User ID=SA;Password=TicketManager!1;TrustServerCertificate=true");
+            optionsBuilder.UseSqlServer("Server=plbie01db11;Database=TicketManager_LAB;User ID=TicketManager_LAB_DBO;Password=TicketManager2024!;TrustServerCertificate=true");
         }
         */
-        
+
     }
 }
