3306c296961a2267285b7b6739b5aca272bd815f
lzlocki <lzlocki@poczta.fm>
2024-08-19 17:27:06 +0200

    update product displacement controller

--- TicketManager.Api/Controllers/ProductDisplacementController.cs
+++ TicketManager.Api/Controllers/ProductDisplacementController.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using TicketManager.Services.ProductDisplacementServices;
+using TicketManager.Services.ProductDisplacement_Services;
 
 namespace TicketManager.Api.Controllers
 {
