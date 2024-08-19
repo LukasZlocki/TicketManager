e698c040e648f37662a104f23d5cef376c97afee
lzlocki <lzlocki@poczta.fm>
2024-08-19 16:54:37 +0200

    add new test parameter model

---
+++ TicketManager.Models/Models/TestParameter.cs
@@ -0,0 +1,11 @@
+﻿namespace TicketManager.Models.Models
+{
+    public class TestParameter
+    {
+        public int TestParameterId { get; set; }
+        public string? ParameterDescription { get; set; }
+        public string? ParameterUnit { get; set; }
+
+        public int TestId { get; set; }
+    }
+}
