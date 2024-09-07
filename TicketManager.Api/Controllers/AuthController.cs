0499e45c44e1ec0134c40dd3978aa902146b98df
lzlocki <lzlocki@poczta.fm>
2024-09-07 17:26:36 +0200

    add empty auth controller

---
+++ TicketManager.Api/Controllers/AuthController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using TicketManager.Services.Department_Services;
+
+namespace TicketManager.Api.Controllers
+{
+    [ApiController]
+    public class AuthController : Controller
+    {
+
+
+    }
+}
